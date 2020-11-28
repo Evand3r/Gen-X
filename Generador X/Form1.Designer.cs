0b95c1a395d8e1b047a3fc00a7a5a963128e94fc
Shamil Carela <sha.carela@gmail.com>
2020-11-28 00:45:42 -0400

    Reordenar paneles, seleccionar paneles, scroll en panel layout

--- Generador X/Form1.Designer.cs
+++ Generador X/Form1.Designer.cs
@@ -29,6 +29,11 @@ namespace Generador_X
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label4;
+            System.Windows.Forms.Label label5;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.roundedButton2 = new Generador_X.Controls.RoundedButton();
             this.roundedButton1 = new Generador_X.Controls.RoundedButton();
@@ -38,9 +43,55 @@ namespace Generador_X
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BttnBajar = new System.Windows.Forms.Button();
+            this.BttnSubir = new System.Windows.Forms.Button();
+            this.BttnAddField = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            label2 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BttnAddField)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(18, 8);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(177, 25);
+            label2.TabIndex = 0;
+            label2.Text = "Nombre del campo";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label4.Location = new System.Drawing.Point(219, 8);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(49, 25);
+            label4.TabIndex = 0;
+            label4.Text = "Tipo";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label5.Location = new System.Drawing.Point(388, 8);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(91, 25);
+            label5.TabIndex = 0;
+            label5.Text = "Opciones";
             // 
             // panel1
             // 
@@ -66,7 +117,7 @@ namespace Generador_X
             this.roundedButton2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.roundedButton2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.roundedButton2.ForeColor = System.Drawing.Color.DimGray;
-            this.roundedButton2.Location = new System.Drawing.Point(137, 10);
+            this.roundedButton2.Location = new System.Drawing.Point(125, 10);
             this.roundedButton2.Name = "roundedButton2";
             this.roundedButton2.Size = new System.Drawing.Size(100, 30);
             this.roundedButton2.TabIndex = 1;
@@ -86,7 +137,7 @@ namespace Generador_X
             this.roundedButton1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.roundedButton1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.roundedButton1.ForeColor = System.Drawing.Color.White;
-            this.roundedButton1.Location = new System.Drawing.Point(10, 10);
+            this.roundedButton1.Location = new System.Drawing.Point(11, 10);
             this.roundedButton1.Name = "roundedButton1";
             this.roundedButton1.Size = new System.Drawing.Size(100, 30);
             this.roundedButton1.TabIndex = 0;
@@ -154,11 +205,116 @@ namespace Generador_X
             // 
             // flowLayoutPanel2
             // 
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.Controls.Add(this.panel4);
+            this.flowLayoutPanel2.Controls.Add(this.panel3);
+            this.flowLayoutPanel2.Controls.Add(this.panel6);
             this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 74);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(784, 571);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(784, 497);
             this.flowLayoutPanel2.TabIndex = 0;
+            this.flowLayoutPanel2.WrapContents = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel4.Location = new System.Drawing.Point(3, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(497, 100);
+            this.panel4.TabIndex = 1;
+            this.panel4.Click += new System.EventHandler(this.panel_Click);
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.style_Selected);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkRed;
+            this.panel3.Location = new System.Drawing.Point(3, 109);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(497, 100);
+            this.panel3.TabIndex = 0;
+            this.panel3.Click += new System.EventHandler(this.panel_Click);
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.style_Selected);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.LimeGreen;
+            this.panel6.Location = new System.Drawing.Point(3, 215);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(497, 100);
+            this.panel6.TabIndex = 2;
+            this.panel6.Click += new System.EventHandler(this.panel_Click);
+            this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.style_Selected);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.BttnBajar);
+            this.panel2.Controls.Add(this.BttnSubir);
+            this.panel2.Controls.Add(this.BttnAddField);
+            this.panel2.Controls.Add(label5);
+            this.panel2.Controls.Add(label4);
+            this.panel2.Controls.Add(label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(784, 50);
+            this.panel2.TabIndex = 1;
+            // 
+            // BttnBajar
+            // 
+            this.BttnBajar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BttnBajar.BackgroundImage")));
+            this.BttnBajar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BttnBajar.Location = new System.Drawing.Point(692, 6);
+            this.BttnBajar.Name = "BttnBajar";
+            this.BttnBajar.Size = new System.Drawing.Size(35, 35);
+            this.BttnBajar.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.BttnBajar, "Mover campo hacia abajo.");
+            this.BttnBajar.UseVisualStyleBackColor = true;
+            this.BttnBajar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BttnSubir
+            // 
+            this.BttnSubir.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BttnSubir.BackgroundImage")));
+            this.BttnSubir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BttnSubir.Location = new System.Drawing.Point(646, 6);
+            this.BttnSubir.Name = "BttnSubir";
+            this.BttnSubir.Size = new System.Drawing.Size(35, 35);
+            this.BttnSubir.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.BttnSubir, "Mover campo hacia arriba.");
+            this.BttnSubir.UseVisualStyleBackColor = true;
+            this.BttnSubir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BttnAddField
+            // 
+            this.BttnAddField.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BttnAddField.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BttnAddField.BackgroundImage")));
+            this.BttnAddField.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BttnAddField.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BttnAddField.Location = new System.Drawing.Point(735, 0);
+            this.BttnAddField.Margin = new System.Windows.Forms.Padding(5);
+            this.BttnAddField.Name = "BttnAddField";
+            this.BttnAddField.Size = new System.Drawing.Size(49, 50);
+            this.BttnAddField.TabIndex = 1;
+            this.BttnAddField.TabStop = false;
+            this.toolTip1.SetToolTip(this.BttnAddField, "Añadir campo.");
+            this.BttnAddField.Click += new System.EventHandler(this.BttnAddField_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.DarkRed;
+            this.panel5.Location = new System.Drawing.Point(3, 109);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(497, 100);
+            this.panel5.TabIndex = 0;
             // 
             // Form1
             // 
@@ -166,16 +322,21 @@ namespace Generador_X
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 661);
             this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Gen X";
             this.panel1.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BttnAddField)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -190,6 +351,17 @@ namespace Generador_X
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox BttnAddField;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button BttnBajar;
+        private System.Windows.Forms.Button BttnSubir;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- Generador X/Form1.cs
+++ Generador X/Form1.cs
@@ -12,9 +12,79 @@ namespace Generador_X
 {
     public partial class Form1 : Form
     {
+        Control focus = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (focus != null)
+                {
+                    flowLayoutPanel2.Controls.SetChildIndex(
+                        focus,
+                        flowLayoutPanel2.Controls.GetChildIndex(focus) - 1
+                        );
+                }
+            }
+            catch (Exception ex)
+            {
+                //lol
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (focus != null)
+                {
+                    flowLayoutPanel2.Controls.SetChildIndex(
+                        focus,
+                        flowLayoutPanel2.Controls.GetChildIndex(focus) + 1
+                        );
+                }
+            }
+            catch (Exception ex)
+            {
+                //lol
+            }
+        }
+
+        private void panel_Click(object sender, EventArgs e)
+        {
+            if (focus != null)
+            {
+                focus.Invalidate();
+            }
+
+            focus = sender as Control;
+            focus.Invalidate();
+        }
+
+        private void style_Selected(object sender, PaintEventArgs e)
+        {
+            if (sender == focus)
+            {
+                Color col = Color.DarkBlue;
+                ButtonBorderStyle bbs = ButtonBorderStyle.Solid;
+                int ancho = 4;
+
+                ControlPaint.DrawBorder(e.Graphics, this.focus.ClientRectangle, col, ancho, bbs,
+                                                                                col, ancho, bbs,
+                                                                                col, ancho, bbs,
+                                                                                col, ancho, bbs);
+            }
+        }
+
+        private void BttnAddField_Click(object sender, EventArgs e)
+        {
+            // Mostrar modal con los diferentes tipos de campos.
+            // Al elegir, scroll layoutpanel2 hasta el fondo
         }
     }
 }
