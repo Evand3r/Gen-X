3721782eb292cfab5b54e3faa12e0e092b166585
Shamil Carela <sha.carela@gmail.com>
2020-12-11 22:21:58 -0400

    Añadir configuracion de idioma, valor por defecto "es"

---
+++ Generador X/SettingsForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Generador_X
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CBLanguages = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Idioma de salida";
+            // 
+            // CBLanguages
+            // 
+            this.CBLanguages.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CBLanguages.FormattingEnabled = true;
+            this.CBLanguages.Location = new System.Drawing.Point(122, 43);
+            this.CBLanguages.Name = "CBLanguages";
+            this.CBLanguages.Size = new System.Drawing.Size(121, 23);
+            this.CBLanguages.TabIndex = 1;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 99);
+            this.Controls.Add(this.CBLanguages);
+            this.Controls.Add(this.label1);
+            this.Name = "SettingsForm";
+            this.Text = "Configuracion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CBLanguages;
+    }
+}
---
+++ Generador X/SettingsForm.cs
@@ -0,0 +1,41 @@
+﻿using Generador_X.Model;
+using Generador_X.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Generador_X
+{
+    public partial class SettingsForm : Form
+    {
+        public SettingsForm()
+        {
+            InitializeComponent();
+
+            CBLanguages.DataSource = new BindingSource(Languages.list, null);
+            CBLanguages.DisplayMember = "key";
+            CBLanguages.ValueMember = "Value";
+
+            CBLanguages.SelectedItem = Languages.list.FirstOrDefault(x => x.Value == Settings1.Default.Idioma);
+            CBLanguages.SelectedIndexChanged += CBLanguages_SelectedValueChanged;
+        }
+
+        public SettingsForm(string WName)
+        {
+            Text = WName;
+        }
+
+        private void CBLanguages_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (CBLanguages.SelectedValue.ToString() != Settings1.Default.Idioma)
+            {
+                Settings1.Default.Idioma = CBLanguages.SelectedValue.ToString();
+            }
+        }
+    }
+}
