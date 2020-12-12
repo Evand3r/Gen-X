709128f2e9632b134b55e279ce7217b06919e57c
Shamil Carela <sha.carela@gmail.com>
2020-12-11 22:27:07 -0400

    Agregar lista de idiomas

---
+++ Generador X/Model/Languages.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generador_X.Model
+{
+    class Languages
+    {
+        public static Dictionary<string, string> list = new Dictionary<string, string>
+        {
+            {"Afrikaans","af_ZA"},
+            {"Árabe","ar"},
+            {"Azerbaiyano","az"},
+            {"Checo","cz"},
+            {"Aleman","de"},
+            {"Aleman (Austria)","de_AT"},
+            {"Aleman (Suiza)","de_CH"},
+            {"Griego","el"},
+            {"Inglés","en"},
+            {"Inglés (Australia)","en_AU"},
+            {"Inglés (Australia Ocker)","en_au_ocker"},
+            {"Inglés (Bork)","en_BORK"},
+            {"Inglés (Canada)","en_CA"},
+            {"Inglés (Gran Bretaña)","en_GB"},
+            {"Inglés (Irlanda)","en_IE"},
+            {"Inglés (India)","en_IND"},
+            {"Nigeria (Inglés)","en_NG"},
+            {"Inglés (Estados Unidos)","en_US"},
+            {"Inglés (SudAfrica)","en_ZA"},
+            {"Español","es"},
+            {"Español (Mexico)","es_MX"},
+            {"Farsi","fa"},
+            {"Finlandés","fi"},
+            {"Francés","fr"},
+            {"Francés (Canada)","fr_CA"},
+            {"Francés (Suiza)","fr_CH"},
+            {"Georgiano","ge"},
+            {"Croata","hr"},
+            {"Indonesia","id_ID"},
+            {"Italiano","it"},
+            {"Japonés","ja"},
+            {"Koreano","ko"},
+            {"Letón","lv"},
+            {"Noruego","nb_NO"},
+            {"Nepalí","nep"},
+            {"Neerlandés","nl"},
+            {"Neerlandés (Bélgica)","nl_BE"},
+            {"Polaco","pl"},
+            {"Portugués (Brazil)","pt_BR"},
+            {"Portugués (Portugal)","pt_PT"},
+            {"Rumano","ro"},
+            {"Ruso","ru"},
+            {"Eslovaco","sk"},
+            {"Sueco","sv"},
+            {"Turco","tr"},
+            {"Ucraniano","uk"},
+            {"Vietnamita","vi"},
+            {"Chino","zh_CN"},
+            {"Chino (Taiwan)","zh_TW"},
+            {"Zulu (SudAfrica)","zu_ZA"},
+        };
+    }
+}
